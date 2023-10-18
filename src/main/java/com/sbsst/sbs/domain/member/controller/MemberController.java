7bdc7bad20c36c3a2def1c1e2b2358991860a697
gsw05018 <gsw05018@naver.com>
2023-10-18 16:28:55 +0900

    Post login method

--- src/main/java/com/sbsst/sbs/domain/member/controller/MemberController.java
+++ src/main/java/com/sbsst/sbs/domain/member/controller/MemberController.java
@@ -1,10 +1,14 @@
 package com.sbsst.sbs.domain.member.controller;
 
+import com.sbsst.sbs.domain.member.entity.Member;
 import com.sbsst.sbs.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usr/member")
@@ -15,6 +19,24 @@ public class MemberController {
     @GetMapping("/login")
     public String showLogin(){
         return "usr/member/login";
+    }
+
+    @PostMapping("login")
+    public String login(String username, String password){
+        Optional<Member> opMember = memberService.findByUsername(username);
+        // 주어진 username에 해당하는 member를 가져옴
+        
+        if(opMember.isEmpty()){
+            return "redirect:/usr/member/login?error";
+        } // 존재하지 않는 경우 해당경로 이동
+
+        Member member = opMember.get();
+        // Optional 객체에서 Member 객체 추출
+
+        if( member.getPassword().equals(password) == false){
+            return "redirect:/usr/member/login?error";
+        } // 입력한 비밀번호가 오류 시 오류페이지 이동
+        return "redirect:/"; // 성공한경우 메인페이지 이동
     }
 
     @GetMapping("/me")
